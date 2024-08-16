8458b84ce6506211b62b0056417db7f09ed3d7dd
sbakhmetzyanov <serezha.akhmetzyanov@yandex.ru>
2024-08-16 15:46:39 +0300

    hw4Selenoid

--- conftest.py
+++ conftest.py
@@ -36,13 +36,12 @@ def browser(request):
     headless_mode = request.config.getoption("--headless")
     url = request.config.getoption("--url")
     log_level = request.config.getoption("--log_level")
-
+    log_directory = "tests/logs/"
     logger = logging.getLogger(request.node.name)
-    file_handler = logging.FileHandler(f"logs/{request.node.name}.log")
+    file_handler = logging.FileHandler(f"../{log_directory}/{request.node.name}.log")
     file_handler.setFormatter(logging.Formatter('%(levelname)s %(message)s'))
     logger.addHandler(file_handler)
     logger.setLevel(level=log_level)
-
     logger.info("===> Test %s started at %s" % (request.node.name, datetime.datetime.now()))
 
     if browser_name == "ch":
